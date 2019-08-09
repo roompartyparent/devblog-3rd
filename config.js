710087fdc492ebfd2899ec6ed91ba591c0f5aae0
roompartyparent <52709683+roompartyparent@users.noreply.github.com>
2019-08-09 16:49:54 -0500

    starting to change stuff

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: 'Room Party Parent - Blog', // Required
+  author: 'Room Party Parent - Blog', // Required
+  description: 'Party With Class',
+  primaryColor: '#1E1548', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://roompartyparent.com',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://roompartyparent-blog.com',
 };
